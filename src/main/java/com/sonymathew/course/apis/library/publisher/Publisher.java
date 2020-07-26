84a619812cda67d467fd0a00c5c2e456163b6249
Sony Mathew <mailsonymathew@gmail.com>
2020-07-26 10:49:40 +0530

    Publisher Request Validation API Working

--- src/main/java/com/sonymathew/course/apis/library/publisher/Publisher.java
+++ src/main/java/com/sonymathew/course/apis/library/publisher/Publisher.java
@@ -1,10 +1,24 @@
 package com.sonymathew.course.apis.library.publisher;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+// This is the model class which we will expose to the consumers. Thsi is linked to the entoty class which is hidden from the consumers.
+// Note : Input Validation constraints will only work if we add dependency  'spring-boot-starter-validation' in POM
+// For the validation work, we also have to use the @Valid annotation in the controller class methods where we use the Publisher object as teh Request body
 public class Publisher {
 	
 	private Integer publisherID;
+	
+	
+	@Size(min=3,max=50,message="Publisher name should be atleast 3 chars and not bigger than 50 chars" )
 	private String name;
+	
+	@Email(message="Please enter a valid Email id")
 	private String emailId;
+	
+	@Pattern(regexp = "\\d{3}-\\d{3}-\\d{3}", message ="Please enter phone number is format 123-456-789")
 	private String phoneNumber;
 	
 	public Publisher() {
