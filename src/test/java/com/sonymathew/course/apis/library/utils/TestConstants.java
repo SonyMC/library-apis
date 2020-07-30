b58957712a59351991d1c98e18440f708939e6e8
Sony Mathew <mailsonymathew@gmail.com>
2020-07-30 18:17:14 +0530

    Publisher API Unit Tests

---
+++ src/test/java/com/sonymathew/course/apis/library/utils/TestConstants.java
@@ -0,0 +1,12 @@
+package com.sonymathew.course.apis.library.utils;
+
+public class TestConstants {
+	
+	public static final String API_TRACE_ID = "Test-Trace-Id-5544" ;
+	public static final String TEST_PUBLISHER_NAME = "TestPublisherName";
+	public static final String TEST_PUBLISHER_EMAIL = "TestPublisherName@email.com";
+	public static final String TEST_PUBLISHER_PHONE = "111-222-333";	
+	public static final String TEST_PUBLISHER_EMAIL_UPDATED = "TestPublisherNameUpdated@email.com";
+	public static final String TEST_PUBLISHER_PHONE_UPDATED = "111-222-444";
+
+}
